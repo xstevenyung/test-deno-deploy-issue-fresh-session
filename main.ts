7d68298c63af1d9ecf7cc605b725e9b8c8be1b02
Steven Yung <stevenyung@fastmail.com>
2022-08-30 23:43:03 +0200

    init

---
+++ main.ts
@@ -0,0 +1,11 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
+
+Deno.env.set("APP_KEY", "secret");
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+await start(manifest);
---
+++ routes/index.tsx
@@ -0,0 +1,18 @@
+/** @jsx h */
+import { h } from "preact";
+
+export default function () {
+  return (
+    <div>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p>
+        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
+        file, and refresh.
+      </p>
+    </div>
+  );
+}
